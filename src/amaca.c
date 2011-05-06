9aff9fe889353b206e321d12689c173913531b44
Alessandro Ghedini <al3xbio@gmail.com>
2011-05-07 00:10:44 +0200

    use 'return' instead of 'print' to get result from code

--- src/amaca.c
+++ src/amaca.c
@@ -36,14 +36,12 @@
 
 #define TMPL_START "{{"
 #define TMPL_END   "}}"
-#define TMPL_VAR   "__Amaca_tmpl__"
 
 #define check_value(x) if (x == NULL) return NULL;
 
 char *Amaca_template(const char *template, ...);
 char *Amaca_template_file(const char *filename, ...);
 
-static int   lua_print(lua_State *l);
 static char *lua_exec(char *code, va_list args);
 static char *eval_template(const char *template, va_list args);
 static char *str_replace(char *orig, char *str, char *start, char *end);
@@ -150,19 +148,6 @@ static char *lua_exec(char *code, va_list args) {
 	lua_State *l = luaL_newstate();
 	luaL_openlibs(l);
 
-	/* override built-in lua "print()" function */
-	lua_register(l, "print", lua_print);
-
-	/*
-	 * initialize (empty) the current code output global variable TMPL_VAR
-	 *
-	 * this may be avoided by adding the proper checks in "lua_print()"
-	 * to avoid a segfault.
-	 */
-
-	lua_pushstring(l, "");
-	lua_setglobal(l, TMPL_VAR);
-
 	/* pass arguments to lua */
 	va_copy(args_copy, args); /* do not modify "args", it is reused */
 
@@ -187,60 +172,10 @@ static char *lua_exec(char *code, va_list args) {
 	}
 
 	/* extract code block result */
-	lua_getglobal(l, TMPL_VAR);
 	tmpl = (char *) lua_tostring(l, -1);
 
 	lua_settop(l, 0);
+	lua_close(l);
 
 	return tmpl;
-}
-
-static int lua_print(lua_State *l) {
-	const char *tmpl;
-	char *result = NULL;
-	int i, args = lua_gettop(l);
-
-	lua_getglobal(l, "tostring");
-
-	/* merge print arguments in a single buffer */
-	for(i = 1; i <= args; i++) {
-		const char *s;
-
-		lua_pushvalue(l, -1);
-		lua_pushvalue(l, i);
-		lua_call(l, 1, 1);
-		s = lua_tostring(l, -1);
-
-		if (result == NULL) {
-			result = (char *) malloc(strlen(s) + 1);
-			strcpy(result, s);
-		} else {
-			result = (char *) realloc(result, strlen(result) + strlen(s) + 1);
-			result = strcat(result, s);
-		}
-
-		lua_pop(l, 1);
-	}
-
-	/*
-	 * update lua template result variable
-	 *
-	 * the TMPL_VAR global variable is kept inside lua state, containing the
-	 * output of the lua code; at every "print()" call the print arguments
-	 * are appended to this variable and at the end of the code execution,
-	 * the variable is extracted and is used to replace the code block in
-	 * the template.
-	 */
-
-	lua_getglobal(l, TMPL_VAR);
-	tmpl = lua_tostring(l, -1);
-
-	result = (char *) realloc(result, strlen(tmpl) + strlen(result) + 1);
-
-	result = strcat((char *)tmpl, result);
-
-	lua_pushstring(l, result);
-	lua_setglobal(l, TMPL_VAR);
-
-	return 0;
 }
