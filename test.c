37c266c17c385777592d0556c367fe51c00ff9d8
Alessandro Ghedini <al3xbio@gmail.com>
2011-12-17 13:29:32 +0100

    add tests for amaca_eval_fd and amaca_veval_fd

--- test.c
+++ test.c
@@ -3,6 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 #include "src/amaca.h"
 
 static unsigned int run = 0, failed = 0;
@@ -61,6 +65,19 @@ void test_eval_file() {
 	free(result);
 }
 
+void test_eval_fd() {
+	char *result;
+	char *title = "This is a title";
+	char *body  = "This is the body";
+	int fd = open("eg/example.tmpl", O_RDONLY);
+
+	test_diag("Test fd");
+	result = amaca_eval_fd(fd, 2, "title", title, "body", body);
+	test_cond(strcmp(result, "<!DOCTYPE html>\n\n<head>\n\t<title>This is a title</title>\n</head>\n\n<body>\n\t<h1>This is the body</h1>\n\t\n\t\t<p>Number 10</p>\n\n\t\t<p>Number 9</p>\n\n\t\t<p>Number 8</p>\n\n\t\t<p>Number 7</p>\n\n\t\t<p>Number 6</p>\n\n\t\t<p>Number 5</p>\n\n\t\t<p>Number 4</p>\n\n\t\t<p>Number 3</p>\n\n\t\t<p>Number 2</p>\n\n\t\t<p>Number 1</p>\n\n</body>\n</html>\n") == 0);
+	free(result);
+	close(fd);
+}
+
 void test_veval_simple_helper(const char *template, int nargs, va_list args) {
 	char *result;
 
@@ -96,14 +113,36 @@ void test_veval_file(int nargs, ...) {
 	va_end(args);
 }
 
+void test_veval_fd_helper(const int fd, int nargs, va_list args) {
+	char *result;
+
+	test_diag("Test vtemplate fd");
+	result = amaca_veval_fd(fd, 2, args);
+	test_cond(strcmp(result, "<!DOCTYPE html>\n\n<head>\n\t<title>This is a title</title>\n</head>\n\n<body>\n\t<h1>This is the body</h1>\n\t\n\t\t<p>Number 10</p>\n\n\t\t<p>Number 9</p>\n\n\t\t<p>Number 8</p>\n\n\t\t<p>Number 7</p>\n\n\t\t<p>Number 6</p>\n\n\t\t<p>Number 5</p>\n\n\t\t<p>Number 4</p>\n\n\t\t<p>Number 3</p>\n\n\t\t<p>Number 2</p>\n\n\t\t<p>Number 1</p>\n\n</body>\n</html>\n") == 0);
+	free(result);
+}
+
+void test_veval_fd(int nargs, ...) {
+	va_list args;
+	int fd = open("eg/example.tmpl", O_RDONLY);
+
+	va_start(args, nargs);
+	test_veval_fd_helper(fd, nargs, args);
+	va_end(args);
+
+	close(fd);
+}
+
 int main() {
 	putchar('\n');
 
 	test_eval_simple();
 	test_eval_multiple();
 	test_eval_file();
+	test_eval_fd();
 	test_veval_simple(2, "arg1", "fir", "arg2", "st");
 	test_veval_file(2, "title", "This is a title", "body", "This is the body");
+	test_veval_fd(2, "title", "This is a title", "body", "This is the body");
 
 	if (failed > 0) {
 		printf("\n%d/%d TESTS HAVE FAILED!\n", failed, run);
